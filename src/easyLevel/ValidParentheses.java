000bd7024ca1b40d06b2028a85390580a4a394b2
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-03-10 00:41:41 +0530

    initial commit

---
+++ src/easyLevel/ValidParentheses.java
@@ -0,0 +1,40 @@
+package easyLevel;
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 17 version
+ * Date: 09-Mar-22
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+        System.out.println("Enter the string:");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(validParentheses.isValid(s));
+
+    }
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+
+
+    }
+}
